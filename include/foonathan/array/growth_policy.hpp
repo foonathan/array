35e8dd2855e98e517e9843bf7129a724ee78be0c
Jonathan Müller <git@foonathan.net>
2018-04-04 13:57:31 +0200

    Improve growth policy calculation

--- include/foonathan/array/growth_policy.hpp
+++ include/foonathan/array/growth_policy.hpp
@@ -32,24 +32,88 @@ namespace foonathan
             }
         };
 
-        /// A growth policy where it grows by the specified factor.
+        namespace detail
+        {
+            // scales by GrowthFactor
+            template <unsigned GrowthFactor>
+            struct whole_growth
+            {
+                static_assert(GrowthFactor > 1, "does not actually grow the size");
+
+                static size_type grow(size_type cur_size) noexcept
+                {
+                    return GrowthFactor * cur_size;
+                }
+            };
+
+            // scales by GrowthNumerator / 2, rounding down
+            template <unsigned GrowthNumerator>
+            struct uneven_halfs_growth
+            {
+                static_assert(GrowthNumerator > 2, "does not actually grow the size");
+
+                static constexpr auto factor = GrowthNumerator / 2;
+
+                static size_type grow(size_type cur_size) noexcept
+                {
+                    // we want: GrowthNumerator / 2 * cur_size
+                    //        = GrowthNumerator * cur_size / 2
+                    // we know: cur_size / 2 = floor(cur_size / 2) + (cur_size mod 2) * 0.5
+                    // so insert that:
+                    //        = GrowthNumerator * (floor(cur_size / 2) + (cur_size mod 2) * 0.5)
+                    // factor out:
+                    //        = GrowthNumerator * floor(cur_size / 2) + GrowthNumerator * (cur_size mod 2) * 0.5
+                    // and rearrange:
+                    //        = GrowthNumerator * floor(cur_size / 2) + GrowthNumerator * (cur_size mod 2) / 2
+                    //        = GrowthNumerator * floor(cur_size / 2) + factor * (cur_size mod 2)
+
+                    auto half = cur_size / 2; // floor(cur_size / 2)
+                    auto rem  = cur_size % 2; // cur_size mod 2
+                    return GrowthNumerator * half + factor * rem;
+                }
+            };
+
+            // scales by GrowthNumerator / GrowthDenominator, properly rounding
+            template <unsigned GrowthNumerator, unsigned GrowthDenominator>
+            struct frac_growth
+            {
+                static_assert(GrowthNumerator > GrowthDenominator,
+                              "does not actually grow the size");
+
+                static constexpr auto factor = double(GrowthNumerator) / double(GrowthDenominator);
+
+                static size_type grow(size_type cur_size) noexcept
+                {
+                    return size_type(factor * double(cur_size) + 0.5);
+                }
+            };
+
+            template <unsigned GrowthNumerator, unsigned GrowthDenominator>
+            // clang-format off
+            using select_growth =
+                typename std::conditional<GrowthNumerator % GrowthDenominator == 0,
+                                              whole_growth<GrowthNumerator>, // whole number
+                typename std::conditional<GrowthDenominator == 2,
+                                              uneven_halfs_growth<GrowthNumerator>, // n / 2
+                                        // else:
+                                              frac_growth<GrowthNumerator, GrowthDenominator> // n / d
+                >::type>::type;
+            // clang-format on
+        } // namespace detail
+
+        /// A growth policy where it grows by the specified factor `GrowthNumerator / GrowthDenominator`.
         template <unsigned GrowthNumerator, unsigned GrowthDenominator = 1>
         class factor_growth
         {
-            using factor_type = typename std::conditional<GrowthNumerator % GrowthDenominator == 0,
-                                                          size_type, double>::type;
+            using growth = detail::select_growth<GrowthNumerator, GrowthDenominator>;
 
         public:
-            static constexpr auto growth_factor = factor_type(GrowthNumerator) / GrowthDenominator;
-
             /// \returns The current size multiplied by the growth factor,
             /// or the current size plus the additional one, whatever is larger.
             static size_type growth_size(size_type cur_size, size_type additional_needed) noexcept
             {
-                auto needed = cur_size + additional_needed;
-                // todo: make this one nicer
-                auto factored_growth =
-                    static_cast<size_type>(growth_factor * static_cast<factor_type>(cur_size));
+                auto needed          = cur_size + additional_needed;
+                auto factored_growth = growth::grow(cur_size);
                 return factored_growth > needed ? factored_growth : needed;
             }
 
@@ -63,7 +127,7 @@ namespace foonathan
 
         /// The default growth policy.
         using default_growth = factor_growth<2>;
-    }
-} // namespace foonathan::array
+    } // namespace array
+} // namespace foonathan
 
 #endif // FOONATHAN_ARRAY_GROWTH_POLICY_HPP_INCLUDED
--- test/growth_policy.cpp
+++ test/growth_policy.cpp
@@ -20,7 +20,6 @@ TEST_CASE("no_extra_growth", "[GrowthPolicy]")
 TEST_CASE("factor_growth", "[GrowthPolicy]")
 {
     using factor1dot5 = factor_growth<3, 2>;
-    static_assert(factor1dot5::growth_factor == 1.5f, "");
 
     REQUIRE(factor1dot5::growth_size(0u, 4u) == 4u);
     REQUIRE(factor1dot5::growth_size(4u, 1u) == 6u);
@@ -31,7 +30,6 @@ TEST_CASE("factor_growth", "[GrowthPolicy]")
     REQUIRE(factor1dot5::shrink_size(8u, 8u) == 8u);
 
     using factor2 = factor_growth<2>;
-    static_assert(factor2::growth_factor == 2, "");
 
     REQUIRE(factor2::growth_size(0u, 4u) == 4u);
     REQUIRE(factor2::growth_size(4u, 1u) == 8u);
@@ -39,4 +37,36 @@ TEST_CASE("factor_growth", "[GrowthPolicy]")
 
     REQUIRE(factor2::shrink_size(4u, 2u) == 2u);
     REQUIRE(factor2::shrink_size(8u, 8u) == 8u);
+
+    SECTION("whole_growth")
+    {
+        REQUIRE(detail::whole_growth<4u>::grow(2u) == 8u);
+        REQUIRE(detail::whole_growth<3u>::grow(2u) == 6u);
+    }
+    SECTION("uneven_halfs_growth")
+    {
+        REQUIRE(detail::uneven_halfs_growth<5u>::grow(1u) == 2u);   // 2.5
+        REQUIRE(detail::uneven_halfs_growth<5u>::grow(2u) == 5u);   // 5
+        REQUIRE(detail::uneven_halfs_growth<5u>::grow(3u) == 7u);   // 7.5
+        REQUIRE(detail::uneven_halfs_growth<5u>::grow(4u) == 10u);  // 10
+        REQUIRE(detail::uneven_halfs_growth<5u>::grow(5u) == 12u);  // 12.5
+        REQUIRE(detail::uneven_halfs_growth<5u>::grow(11u) == 27u); // 27.5
+        REQUIRE(detail::uneven_halfs_growth<5u>::grow(32u) == 80u); // 80
+
+        REQUIRE(detail::uneven_halfs_growth<11u>::grow(1u) == 5u);    // 5.5
+        REQUIRE(detail::uneven_halfs_growth<11u>::grow(2u) == 11u);   // 11
+        REQUIRE(detail::uneven_halfs_growth<11u>::grow(3u) == 16u);   // 16.5
+        REQUIRE(detail::uneven_halfs_growth<11u>::grow(4u) == 22u);   // 22
+        REQUIRE(detail::uneven_halfs_growth<11u>::grow(5u) == 27u);   // 27.5
+        REQUIRE(detail::uneven_halfs_growth<11u>::grow(11u) == 60u);  // 60.5
+        REQUIRE(detail::uneven_halfs_growth<11u>::grow(32u) == 176u); // 176
+    }
+    SECTION("frac_growth")
+    {
+        REQUIRE(detail::frac_growth<5, 3>::grow(1u) == 2); // 1.6..
+        REQUIRE(detail::frac_growth<5, 3>::grow(2u) == 3); // 3.3..
+        REQUIRE(detail::frac_growth<5, 3>::grow(3u) == 5); // 5
+        REQUIRE(detail::frac_growth<5, 3>::grow(4u) == 7); // 6.6..
+        REQUIRE(detail::frac_growth<5, 3>::grow(5u) == 8); // 8.3..
+    }
 }
